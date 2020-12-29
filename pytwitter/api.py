d2850dea0c07e8e0d0124172d4264d16b87fe9c0
ikaroskun <merle.liukun@gmail.com>
2020-12-29 22:04:28 +0800

    WIP(api): :sparkles: add api for follows

--- pytwitter/api.py
+++ pytwitter/api.py
@@ -94,6 +94,10 @@ class Api:
         if "errors" in data:
             raise PyTwitterError(data["errors"])
 
+        # v1 token not
+        if "reason" in data:
+            raise PyTwitterError(data)
+
         return data
 
     def get_users(
@@ -318,5 +322,109 @@ class Api:
         else:
             return (
                 [md.Tweet.new_from_json_dict(tweet) for tweet in tweets],
+                md.Includes.new_from_json_dict(includes),
+            )
+
+    def get_following(
+        self,
+        user_id: Optional[str] = None,
+        *,
+        expansions: Optional[Union[str, List, Tuple]] = None,
+        user_fields: Optional[Union[str, List, Tuple]] = None,
+        tweet_fields: Optional[Union[str, List, Tuple]] = None,
+        max_results: Optional[int] = None,
+        pagination_token: Optional[str] = None,
+        return_json: bool = False,
+    ):
+        """
+        Returns a list of users the specified user ID is following.
+
+        :param user_id: The user ID whose following you would like to retrieve.
+        :param expansions: Fields for the expansions.
+        :param user_fields: Fields for the user object.
+        :param tweet_fields: Fields for the tweet object.
+        :param max_results: The maximum number of results to be returned per page. Number between 1 and the 1000.
+        By default, each page will return 100 results.
+        :param pagination_token: Token for the pagination.
+        :param return_json: Type for returned data. If you set True JSON data will be returned.
+        :return:
+            - data: data for the following.
+            - includes: expansions data.
+        """
+
+        args = {
+            "expansions": enf_comma_separated(name="expansions", value=expansions),
+            "user.fields": enf_comma_separated(name="user_fields", value=user_fields),
+            "tweet.fields": enf_comma_separated(
+                name="tweet_fields", value=tweet_fields
+            ),
+            "max_results": max_results,
+            "pagination_token": pagination_token,
+        }
+
+        resp = self._request(
+            url=f"{self.BASE_URL_V2}/users/{user_id}/following",
+            params=args,
+        )
+        data = self._parse_response(resp)
+        print(data)
+
+        user, includes = data["data"], data.get("includes")
+        if return_json:
+            return user, includes
+        else:
+            return (
+                md.User.new_from_json_dict(user),
+                md.Includes.new_from_json_dict(includes),
+            )
+
+    def get_followers(
+        self,
+        user_id: Optional[str] = None,
+        *,
+        expansions: Optional[Union[str, List, Tuple]] = None,
+        user_fields: Optional[Union[str, List, Tuple]] = None,
+        tweet_fields: Optional[Union[str, List, Tuple]] = None,
+        max_results: Optional[int] = None,
+        pagination_token: Optional[str] = None,
+        return_json: bool = False,
+    ):
+        """
+        Returns a list of users who are followers of the specified user ID.
+
+        :param user_id: The user ID whose following you would like to retrieve.
+        :param expansions: Fields for the expansions.
+        :param user_fields: Fields for the user object.
+        :param tweet_fields: Fields for the tweet object.
+        :param max_results: The maximum number of results to be returned per page. Number between 1 and the 1000.
+        By default, each page will return 100 results.
+        :param pagination_token: Token for the pagination.
+        :param return_json: Type for returned data. If you set True JSON data will be returned.
+        :return:
+            - data: data for the following.
+            - includes: expansions data.
+        """
+        args = {
+            "expansions": enf_comma_separated(name="expansions", value=expansions),
+            "user.fields": enf_comma_separated(name="user_fields", value=user_fields),
+            "tweet.fields": enf_comma_separated(
+                name="tweet_fields", value=tweet_fields
+            ),
+            "max_results": max_results,
+            "pagination_token": pagination_token,
+        }
+
+        resp = self._request(
+            url=f"{self.BASE_URL_V2}/users/{user_id}/followers",
+            params=args,
+        )
+        data = self._parse_response(resp)
+
+        user, includes = data["data"], data.get("includes")
+        if return_json:
+            return user, includes
+        else:
+            return (
+                md.User.new_from_json_dict(user),
                 md.Includes.new_from_json_dict(includes),
             )
