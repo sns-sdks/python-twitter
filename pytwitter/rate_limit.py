4bc949661ee0666adf37a7221e549c088dbe6b11
ikaros <merle.liukun@gmail.com>
2021-03-01 21:28:53 +0800

    feat(ratelimit): :sparkles: add ratelimit for hide reply

--- pytwitter/rate_limit.py
+++ pytwitter/rate_limit.py
@@ -65,6 +65,12 @@ TWEETS_ID_SHOW = Endpoint(
     app_limit=300,
     user_limit=900,
 )
+TWEETS_ID_HIDDEN = Endpoint(
+    resource="/tweets/:id/hidden",
+    regex=re.compile(r"/tweets/\d+/hidden"),
+    app_limit=0,
+    user_limit=50,
+)
 
 PATH_VAR_ENDPOINTS = [
     USER_ID_SHOW,
