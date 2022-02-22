5192b723d05dd97a542baeca8366453510541492
ikaros <merle.liukun@gmail.com>
2022-02-22 23:19:55 +0800

    test(spaces): :white_check_mark: update tests for spaces
    ISSUES CLOSED: #107

--- tests/apis/test_space.py
+++ tests/apis/test_space.py
@@ -60,10 +60,46 @@ def test_search_spaces(api, helpers):
 
     resp = api.search_spaces(
         query="hello",
-        state="live",
         space_fields="title,host_ids",
     )
     assert len(resp.data) == 2
     assert resp.data[0].id == "1DXxyRYNejbKM"
     assert resp.data[0].state == "live"
     assert resp.meta.result_count == 2
+
+
+@responses.activate
+def test_get_buyers_by_space(api, helpers):
+    space_id = "1DXxyRYNejbKM"
+    responses.add(
+        responses.GET,
+        url=f"https://api.twitter.com/2/spaces/{space_id}/buyers",
+        json=helpers.load_json_data("testdata/apis/space/buyers_by_space.json"),
+    )
+
+    resp = api.get_buyers_by_space(
+        space_id=space_id,
+        user_fields="created_at",
+        expansions="pinned_tweet_id",
+        tweet_fields="created_at",
+    )
+    assert len(resp.data) == 2
+    assert resp.data[0].id == "2244994945"
+
+
+@responses.activate
+def test_get_tweets_by_space(api, helpers):
+    space_id = "1DXxyRYNejbKM"
+    responses.add(
+        responses.GET,
+        url=f"https://api.twitter.com/2/spaces/{space_id}/tweets",
+        json=helpers.load_json_data("testdata/apis/space/tweets_by_space.json"),
+    )
+
+    resp = api.get_tweets_by_space(
+        space_id=space_id,
+        expansions="author_id",
+        user_fields="created_at,description",
+    )
+    assert len(resp.data) == 3
+    assert resp.data[0].id == "1389270063807598594"
