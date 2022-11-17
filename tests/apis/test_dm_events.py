534f2b83443b4fb201a9c0c2a1dda15608b176d0
ikaroskun <merle.liukun@gmail.com>
2022-11-17 19:17:31 +0800

    test(dm): :white_check_mark: update tests for send events

--- tests/apis/test_dm_events.py
+++ tests/apis/test_dm_events.py
@@ -58,3 +58,55 @@ def test_get_dm_events(api_with_user, helpers):
         dm_event_fields="id,text,event_type,dm_conversation_id,created_at,sender_id,attachments,participant_ids,referenced_tweets",
     )
     assert resp.data[0].dm_conversation_id == "1585094756761149440"
+
+
+@responses.activate
+def test_create_message_to_participant(api_with_user, helpers):
+    data = helpers.load_json_data("testdata/apis/dm_events/events_create.json")
+    participant_id = "1234567"
+
+    responses.add(
+        responses.POST,
+        url=f"https://api.twitter.com/2/dm_conversations/with/{participant_id}/messages",
+        json=data,
+    )
+    resp = api_with_user.create_message_to_participant(
+        participant_id=participant_id,
+        text="hello",
+        attachments=[{"media_id": "1455952740635586573"}],
+    )
+    assert resp.dm_conversation_id == "1346889436626259968"
+
+
+@responses.activate
+def test_create_message_to_conversation(api_with_user, helpers):
+    data = helpers.load_json_data("testdata/apis/dm_events/events_create.json")
+    dm_conversation_id = "7654321"
+
+    responses.add(
+        responses.POST,
+        url=f"https://api.twitter.com/2/dm_conversations/{dm_conversation_id}/messages",
+        json=data,
+    )
+    resp = api_with_user.create_message_to_conversation(
+        dm_conversation_id=dm_conversation_id,
+        text="hello",
+        attachments=[{"media_id": "1455952740635586573"}],
+    )
+    assert resp.dm_conversation_id == "1346889436626259968"
+
+
+@responses.activate
+def test_create_conversation(api_with_user, helpers):
+    data = helpers.load_json_data("testdata/apis/dm_events/events_create.json")
+    responses.add(
+        responses.POST,
+        url=f"https://api.twitter.com/2/dm_conversations",
+        json=data,
+    )
+    resp = api_with_user.create_conversation(
+        conversation_type="group",
+        message={"text": "hello"},
+        participant_ids=["1234567", "7654321"],
+    )
+    assert resp.dm_conversation_id == "1346889436626259968"
